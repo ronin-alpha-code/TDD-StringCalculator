36890d5501e7fc66c05699ec4207f63c983a79b1
Abhishek <abhishekkumarsikandar@gmail.com>
2025-04-22 23:56:45 +0530

    case 3, multiple numbers separated by comma success

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -4,5 +4,11 @@ class StringCalculator
     return 0 if string.empty?
     ## Condition 2: If the string contains a single number, return that number.
     return string.to_i if string.match?(/^\d+$/) ## /^\d+$/ identifies the single number
+
+    ## Condition 3: If the string contains two numbers separated by a comma, return their sum.
+    numbers = string.split(",").map(&:to_i)
+    sum = 0
+    numbers.each{ |number| sum += number }
+    return sum
   end
 end

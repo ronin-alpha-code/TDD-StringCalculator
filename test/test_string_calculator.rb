b73f12a0301bf31c08b32d3b001834747383b36a
Abhishek <abhishekkumarsikandar@gmail.com>
2025-04-22 23:46:50 +0530

    single number case failure

--- test/test_string_calculator.rb
+++ test/test_string_calculator.rb
@@ -5,4 +5,10 @@ class TestStringCalculator < Minitest::Test
   def test_empty_string_returns_0
     assert_equal 0, StringCalculator.add("")
   end
+
+  def test_single_number_returns_number
+    assert_equal 1, StringCalculator.add("1")
+    assert_equal 2, StringCalculator.add("2")
+    assert_equal 3, StringCalculator.add("3")
+  end
 end

606638252dc1e9f253fb894c0a8fd89cdc698b86
Abhishek <abhishekkumarsikandar@gmail.com>
2025-04-22 23:49:46 +0530

    case 2, single number success

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -2,5 +2,7 @@ class StringCalculator
   def self.add(string)
     ## Condition 1: If the string is empty, return 0.
     return 0 if string.empty?
+    ## Condition 2: If the string contains a single number, return that number.
+    return string.to_i if string.match?(/^\d+$/) ## /^\d+$/ identifies the single number
   end
 end

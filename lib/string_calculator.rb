97daacfc98f9f563cf013520f82d7e11e2a6b22b
Abhishek <abhishekkumarsikandar@gmail.com>
2025-04-23 23:28:18 +0530

    handled if number is greater than 1000

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -10,6 +10,7 @@ class StringCalculator
     numbers.each do |num|
       # raise "Negatives not allowed" if num.to_i < 0 this will raise for first negative number that will be found
       error << "#{num.to_i} Negative not allowed" if num.to_i < 0 ## This will store and return all negative numbers
+      next if num.to_i > 1000 ## This will ignore all numbers greater than 1000
       sum += num.to_i
     end
     error.any? ? (raise error.join(", ")) : sum

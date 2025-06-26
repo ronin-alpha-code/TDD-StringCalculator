89a3ada3ad6a0285b0a272bd70b9bc7e6ed536f8
Abhishek <abhishekkumarsikandar@gmail.com>
2025-06-26 10:57:36 +0530

    feature: Custom delimiters

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -1,18 +1,26 @@
 class StringCalculator
-  def self.add(string)
-    ## Condition 1: If the string is empty, return 0.
+  def add(string)
     return 0 if string.empty?
-    ### Condition if string contains only one number or multiple number separated by comma or separated by new line or any other white space
-    sum = 0
-    error = []
-    string = string.gsub(/\s+/, ",")
-    numbers = string.split(",")
-    numbers.each do |num|
-      # raise "Negatives not allowed" if num.to_i < 0 this will raise for first negative number that will be found
-      error << "#{num.to_i} Negative not allowed" if num.to_i < 0 ## This will store and return all negative numbers
-      next if num.to_i > 1000 ## This will ignore all numbers greater than 1000
-      sum += num.to_i
-    end
-    error.any? ? (raise error.join(", ")) : sum
+    delimiters = fetch_delimiters(string)
+    numbers = fetch_numbers(string, delimiters)
+    negatives = fetch_negatives(numbers)
+    raise "Negatives not allowed: #{negatives.join('-')}" if negatives.any?
+    numbers.select{|num| num <= 1000 }.sum
+  end
+
+  private
+
+  def fetch_delimiters string
+    return "," unless string.start_with?("//")
+    delimiter, input = string[2..].split("\n", 2)
+    [Regexp.escape(delimiter)]
+  end
+
+  def fetch_numbers string, delimiters
+    string.split(/#{Regexp.union(delimiters)}|\n/).map(&:to_i)
+  end
+
+  def fetch_negatives numbers
+    numbers.select { |num| num < 0 }
   end
 end
